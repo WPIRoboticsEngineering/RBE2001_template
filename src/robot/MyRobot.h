74ec3ce80f9ba53805c79a00b95c68f783c2324c
Kevin Harrington <mad.hephaestus@gmail.com>
2018-08-10 12:03:24 -0400

    typos

--- src/robot/MyRobot.h
+++ src/robot/MyRobot.h
@@ -1,14 +1,15 @@
 //A robot class to share data between listeners
 #include "Arduino.h"
-
+#include "RBE2001.h"
 #if !defined(MYROBOT)
 #define MYROBOT
 
 class MyRobot {
 private:
 	String * name;
-	enum RobotStatus myStatus;
 public:
+	enum RobotStatus myStatus;
+
 	MyRobot() ;
 	void loop();
 	String * getName();
