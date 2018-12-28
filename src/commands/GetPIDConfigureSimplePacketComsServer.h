3c111ff875a04b7d79e0d2569328862b29d01067
Kevin Harrington <mad.hephaestus@gmail.com>
2018-12-28 11:48:43 -0500

    Switching to the 2002 template

---
+++ src/commands/GetPIDConfigureSimplePacketComsServer.h
@@ -0,0 +1,21 @@
+/*
+ * PIDConfigureSimplePacketComsServer.h
+ *
+ *  Created on: Oct 19, 2018
+ *      Author: hephaestus
+ */
+
+#ifndef SRC_COMSGet_PIDCONFIGURESIMPLEPACKETCOMSSERVER_H_
+#define SRC_COMGetS_PIDCONFIGURESIMPLEPACKETCOMSSERVER_H_
+#include <SimplePacketComs.h>
+#include "../PIDMotor.h"
+class GetPIDConfigureSimplePacketComsServer: public PacketEventAbstract {
+	PIDMotor ** pidlist;
+	int numPID;
+public:
+	GetPIDConfigureSimplePacketComsServer(int num,PIDMotor ** list);
+	void event(float * buffer);
+
+};
+
+#endif /* SRC_COMS_PIDCONFIGURESIMPLEPACKETCOMSSERVER_H_ */
